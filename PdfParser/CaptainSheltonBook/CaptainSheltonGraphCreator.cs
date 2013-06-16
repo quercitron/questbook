97abcdd6868d3302e2291e719939e41da459ec3d
Sergey <oldquercitron@yandex.ru>
2013-06-17 02:36:02 +0400

    Add Captain Shelton parser

---
+++ PdfParser/CaptainSheltonBook/CaptainSheltonGraphCreator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using GraphCreatorInterface;
+
+namespace CaptainSheltonBook
+{
+    public class CaptainSheltonGraphCreator : BaseGraphCreator
+    {
+        public override BaseGraph CreateGraphFromText(string text)
+        {
+            var graph = new BaseGraph();
+
+            var stream = new MemoryStream(Encoding.ASCII.GetBytes(text));
+            XElement book = XElement.Load(stream);
+
+            foreach (var element in book.Elements("body").Elements("section").Where(s => s.HasAttributes))
+            {
+                var currentParagraph = Convert.ToInt32(element.Element("title").Element("p").Value);
+                var paths = element.Elements("p").Elements("a").Select(path => Convert.ToInt32(path.Value));
+                foreach (var path in paths)
+                {
+                    graph.AddEdge(currentParagraph, path);
+                }
+                var description = string.Join(Environment.NewLine, element.Elements("p").Select(p => p.Value));
+                graph.Descriptions.Add(currentParagraph, description);
+            }
+            return graph;
+        }
+    }
+}
--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using System.Xml.Linq;
 using BaseLib;
 using BaseLib.Enumerables;
 using BlackWoodBook;
+using CaptainSheltonBook;
 using GraphCreatorInterface;
 using PDFBoxParser;
 using PdfParserInterfaces;
@@ -16,52 +18,75 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
+            var writer = File.CreateText("output.txt");
             try
             {
-                /*var book = new Book("blackwood.txt", new BlackWoodGraphCreator(), "Forest Of Doom");
+                //var creator = new CaptainSheltonGraphCreator();
+                //creator.CreateGraphFromFile("Braslavskiy_Tayna_kapitana_Sheltona.240266.fb2");
+                XElement book = XElement.Load("Braslavskiy_Tayna_kapitana_Sheltona.240266.fb2");
+                foreach (var element in book.Elements("body").Elements("section").Where(s => s.HasAttributes))
+                {
+                    writer.WriteLine(element);
+                    writer.WriteLine(element.Name);
+                    writer.WriteLine("# {0}", element.Element("title").Element("p").Value);
+                    var refs = element.Elements("p").Elements("a");
+                    foreach (var @ref in refs)
+                    {
+                        writer.WriteLine(@ref.Value.Trim('(', ')'));
+                    }
+                    var text = string.Join(Environment.NewLine, element.Elements("p").Select(p => p.Value));
+                    writer.WriteLine("({0})", text);
+                }
+                //writer.WriteLine(book.Element("description"));
+            }
+            catch (Exception e)
+            {
+                writer.WriteLine(e);
+            }
+            writer.Dispose();
+        }
+
+        private static void FindNecklace()
+        {
+/*var book = new Book("blackwood.txt", new BlackWoodGraphCreator(), "Forest Of Doom");
                 book.Save("save2.txt");*/
 
-                var book = Book.Load("save.txt");
+            var book = Book.Load("save.txt");
 
-                var edges = book.GetEdges(null, null);
-                string itemName = "Necklace";
-                foreach (var edge in edges)
+            var edges = book.GetEdges(null, null);
+            string itemName = "Necklace";
+            foreach (var edge in edges)
+            {
+                if (edge.RequestedItems.Any(item => item.BasicItem.Name.Contains(itemName)))
                 {
-                    if (edge.RequestedItems.Any(item => item.BasicItem.Name.Contains(itemName)))
-                    {
-                        Console.WriteLine(edge.From.Id);
-                    }
-                    if (edge.RecievedItems.Any(item => item.BasicItem.Name.Contains(itemName)))
-                    {
-                        Console.WriteLine(edge.From.Id);
-                    }
+                    Console.WriteLine(edge.From.Id);
                 }
-
-                foreach (var paragraph in book.Paragraphs)
+                if (edge.RecievedItems.Any(item => item.BasicItem.Name.Contains(itemName)))
                 {
-                    if (paragraph.RequestedItems.Any(item => item.BasicItem.Name.Contains(itemName)))
-                    {
-                        Console.WriteLine(paragraph.Id);
-                    }
-                    if (paragraph.RecievedItems.Any(item => item.BasicItem.Name.Contains(itemName)))
-                    {
-                        Console.WriteLine(paragraph.Id);
-                    }
+                    Console.WriteLine(edge.From.Id);
                 }
+            }
 
-                /*foreach (var item in book.AvailableItems)
+            foreach (var paragraph in book.Paragraphs)
+            {
+                if (paragraph.RequestedItems.Any(item => item.BasicItem.Name.Contains(itemName)))
+                {
+                    Console.WriteLine(paragraph.Id);
+                }
+                if (paragraph.RecievedItems.Any(item => item.BasicItem.Name.Contains(itemName)))
+                {
+                    Console.WriteLine(paragraph.Id);
+                }
+            }
+
+            /*foreach (var item in book.AvailableItems)
                 {
                     item.IsProhibiting = true;
                 }*/
 
-                //book.m_GraphCreator = new BlackWoodGraphCreator();
-                /*book.Update("Майкл Фрост - Чернолесье.doc");*/
-                /*book.Save("save2.txt");*/
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //book.m_GraphCreator = new BlackWoodGraphCreator();
+            /*book.Update("Майкл Фрост - Чернолесье.doc");*/
+            /*book.Save("save2.txt");*/
         }
 
         private static void SearchCycles()
